26b58e6532f2bd1ef376edd789c6a4c5b9b305ac
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-03-04 18:53:12 +0800

    chore: rundata

--- src/main/java/cn/crtlprototypestudios/prma/foundation/data/generators/recipe/ModRecipesGen.java
+++ src/main/java/cn/crtlprototypestudios/prma/foundation/data/generators/recipe/ModRecipesGen.java
@@ -78,16 +78,6 @@ public class ModRecipesGen {
                 .addStep(DeployerApplicationRecipe::new, p -> p.require(PrmaItems.HIGH_POWER_AMMUNITION_GUNPOWDER.get()))
                 .addStep(DeployerApplicationRecipe::new, p -> p.require(PrmaItems.Ammo.FORTY_MIL_SLUG.get()))
                 .addStep(PressingRecipe::new, p -> p)
-                .addOutput(TaczAPIBridge.getAmmo("12g"), 1)
-        );
-
-        addSequencedAssemblyRecipe(new SequencedAssemblyRecipeBuilder(ResourceHelper.find("ammo/40mm"))
-                .require(PrmaItems.Ammo.FORTY_MIL_CASING.get())
-                .transitionTo(PrmaItems.Ammo.FORTY_MIL_CASING.get())
-                .loops(1)
-                .addStep(DeployerApplicationRecipe::new, p -> p.require(PrmaItems.HIGH_POWER_AMMUNITION_GUNPOWDER.get()))
-                .addStep(DeployerApplicationRecipe::new, p -> p.require(PrmaItems.Ammo.FORTY_MIL_SLUG.get()))
-                .addStep(PressingRecipe::new, p -> p)
                 .addOutput(TaczAPIBridge.getAmmo("40mm"), 1)
         );
 
